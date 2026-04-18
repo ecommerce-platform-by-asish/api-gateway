17ccd0dbc64865303d7f2fdb0645ef800b73a94b
asish <beingasishkumar@gmail.com>
2026-04-18 06:29:16 +0530

    fix: enforce Gateway v5 properties, enable Actuator, and add global CORS/timeouts

--- src/main/java/com/app/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/app/apigateway/ApiGatewayApplication.java
@@ -3,7 +3,7 @@ package com.app.apigateway;
 import com.app.common.boot.BaseSpringBootApplication;
 import org.springframework.boot.SpringApplication;
 
-@BaseSpringBootApplication
+@BaseSpringBootApplication(enableActuator = true)
 public class ApiGatewayApplication {
 
   static void main(String[] args) {
